7c3ce153b38b42a5fc3368664e49246f6a9e1c1f
ice1000 <ice1000kotlin@foxmail.com>
2018-04-14 16:47:53 +0800

    [ gradle ] Remove laji codes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,17 @@
-import org.gradle.api.internal.HasConvention
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import java.io.*
 import java.nio.file.*
 import java.util.concurrent.*
 import java.util.stream.Collectors
 
-val commitHash by lazy {
-  val process: Process = Runtime.getRuntime().exec("git rev-parse --short HEAD")
-  process.waitFor()
-  val output = process.inputStream.use {
-    it.bufferedReader().use(BufferedReader::readText)
-  }
-  process.destroy()
-  output.trim()
-}
+val kotlinVersion = "1.2.31"
 
-val isCI = !System.getenv("CI").isNullOrBlank()
-
-val shortVersion = "1.0"
-val packageName = "org.covscript.devkt.lang"
-val kotlinVersion: String by extra
-val pluginCalculatedVersion = if (isCI) "$shortVersion-$commitHash" else shortVersion
-
-group = packageName
-version = pluginCalculatedVersion
-
-buildscript {
-  var kotlinVersion: String by extra
-  kotlinVersion = "1.2.31"
-
-  repositories {
-    mavenCentral()
-  }
-
-  dependencies {
-    classpath(kotlin("gradle-plugin", kotlinVersion))
-  }
-}
-
-tasks.withType<Jar> {
-  manifest {
-    attributes(mapOf("Main-Class" to "org.ice1000.devkt.Main",
-        "SplashScreen-Image" to "splash.png"))
-  }
-}
+group = "org.covscript.devkt.lang"
+version = "v1.0-SNAPSHOT"
 
 plugins {
   java
   kotlin("jvm") version "1.2.31"
-}
-
-apply {
-  plugin("kotlin")
 }
 
 java {
@@ -59,19 +19,20 @@ java {
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val SourceSet.kotlin
-	get() = (this as HasConvention).convention.getPlugin(KotlinSourceSet::class.java).kotlin
-
 java.sourceSets {
   "main" {
     java.setSrcDirs(listOf("src"))
-    kotlin.setSrcDirs(listOf("src"))
+    withConvention(KotlinSourceSet::class) {
+      kotlin.setSrcDirs(listOf("src"))
+    }
     resources.setSrcDirs(listOf("res"))
   }
 
   "test" {
     java.setSrcDirs(emptyList<Any>())
-    kotlin.setSrcDirs(emptyList<Any>())
+    withConvention(KotlinSourceSet::class) {
+      kotlin.setSrcDirs(emptyList<Any>())
+    }
     resources.setSrcDirs(emptyList<Any>())
   }
 }
@@ -86,3 +47,4 @@ dependencies {
   compileOnly(kotlin("compiler-embeddable", kotlinVersion))
   compileOnly(files(*File("lib").listFiles()))
 }
+
